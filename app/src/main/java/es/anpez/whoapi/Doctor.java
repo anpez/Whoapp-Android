0276ed7c1638fbb1295fb883c42858d365506cd4
Antonio Nicolás Pina <antonio@antonionicolaspina.com>
2018-01-27 12:48:56 +0100

    Second class. Basic listview.

---
+++ app/src/main/java/es/anpez/whoapi/Doctor.java
@@ -0,0 +1,9 @@
+package es.anpez.whoapi;
+
+public class Doctor {
+  public String name;
+
+  public Doctor(String name) {
+    this.name = name;
+  }
+}
--- app/src/main/java/es/anpez/whoapi/MainActivity.java
+++ app/src/main/java/es/anpez/whoapi/MainActivity.java
@@ -1,12 +1,19 @@
 package es.anpez.whoapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +22,38 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    final EditText texto = findViewById(R.id.campodetexto);
-    Button boton = findViewById(R.id.boton);
+    List<Doctor> doctors = new ArrayList<>();
+    //doctors.add(new Doctor("William Hartnell"));
+    //doctors.add(new Doctor("David Tennant"));
+for(int i=0;i<1000;i++) {
+  doctors.add(new Doctor("Doctor"+i));
+}
+    ListView list = findViewById(R.id.list);
+    list.setAdapter(new DoctorsAdapter(this, doctors));
+  }
 
-    boton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        String text =  "Hola: " + texto.getText().toString();
-        Toast.makeText(view.getContext(), text, Toast.LENGTH_LONG).show();
+  public class DoctorsAdapter extends ArrayAdapter<Doctor> {
+    public DoctorsAdapter(Context context, List<Doctor> doctors) {
+      super(context, 0, doctors);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+      Doctor doctor = getItem(position);
+
+      if (null == convertView) {
+        Log.d("DoctorsAdapter", "Creando posicion:"+position);
+        convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_doctors_item, parent, false);
+      } else {
+        Log.d("DoctorsAdapter", "Reusando posicion:"+position);
       }
-    });
+
+      TextView tv = convertView.findViewById(R.id.name);
+      tv.setText(doctor.name);
+
+      return convertView;
+    }
   }
 }
