62b1000f2618010920b19ca2aac751bfeab6d13b
Antonio Nicolás Pina <antonio@antonionicolaspina.com>
2018-03-24 13:04:37 +0100

    Collapsing toolbar.

--- app/src/main/java/es/anpez/whoapi/DoctorDetailActivity.java
+++ app/src/main/java/es/anpez/whoapi/DoctorDetailActivity.java
@@ -4,11 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class DoctorDetailActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
 
+public class DoctorDetailActivity extends AppCompatActivity {
   public static final String DOCTOR = "doctor";
 
   public static Intent newIntent(Context context, Doctor doctor) {
@@ -20,7 +25,30 @@ public class DoctorDetailActivity extends AppCompatActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_doctor_detail);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
     Doctor doctor = getIntent().getParcelableExtra(DOCTOR);
     Toast.makeText(this, "Doctor: "+doctor, Toast.LENGTH_LONG).show();
+
+    String url = "http://images.amcnetworks.com/bbcamerica.com/wp-content/uploads/2014/04/davidtennant-1600x720.jpg";
+    ImageView avatarImageView = findViewById(R.id.avatar);
+    Picasso.get().load(url).into(avatarImageView);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId()==android.R.id.home) {
+      supportFinishAfterTransition();
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
